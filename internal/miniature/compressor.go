493c4bc64314209bad61a76f2f3513e21b863a20
Onen Julius <jonen54@gmail.com>
2021-02-28 16:49:35 +0100

    Remove unnecessary logging

--- internal/miniature/compressor.go
+++ internal/miniature/compressor.go
@@ -2,7 +2,6 @@ package miniature
 
 import (
 	"errors"
-	"log"
 
 	"github.com/pierrec/lz4"
 )
@@ -10,9 +9,6 @@ import (
 // Compress compresses bytes passed to it and returns a compressed byte array
 func Compress(data []byte) (compressed []byte, err error) {
 	buff := make([]byte, 100*len(data))
-
-	log.Println("Compress Buffer size", len(buff))
-	log.Println("Data to compress size", len(data))
 	n, err := lz4.CompressBlockHC(data, buff, 1)
 	if err != nil {
 		return nil, err
@@ -30,8 +26,6 @@ func Compress(data []byte) (compressed []byte, err error) {
 // Decompress decompresses bytes passed to it and returns a decompressed byte array
 func Decompress(data []byte) (decompressedData []byte, err error) {
 	buff := make([]byte, 100*len(data))
-	log.Println("Decompress Buffer size", len(buff))
-	log.Println("Data to decompress size", len(data))
 	n, err := lz4.UncompressBlock(data, buff)
 	if err != nil {
 		return nil, err
